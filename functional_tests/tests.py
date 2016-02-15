e2d99171c6432614424dcb1102ec0d5d760614b1
iehyxue <iehyxue@qq.com>
2016-02-15 13:56:27 +0800

    make functional_test an app, use LiveServerTetCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -10,10 +11,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # look up homepage
-        self.browser.get('http://localhost:8000')
-
+        #self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         #see "To-DO"
         self.assertIn('To-Do',self.browser.title)
         header_test = self.browser.find_element_by_tag_name('h1').text
@@ -32,17 +38,8 @@ class NewVisitorTest(unittest.TestCase):
         # press enter,and the page reflash
         # list show "1\buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
 
-        # import time
-        # time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table -- its text was:\n%s" % (
-                table.text,
-            )
-        )
 
         # appear text block on page,can input other items
         # she input "use peacock feathers to make a fly"
@@ -51,18 +48,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # the page reflash again and show these two items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2:Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
 
         #she want to know weather this page can remember her list
         #she sawthis website generate a url
         #on the page there are some text describe this function
-        self.fail('Finish the text!')
-if __name__=='__main__':
-     unittest.main(warnings='ignore')
+        self.fail('Finish the test!')
 
